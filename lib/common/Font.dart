4badd4b546b26135b1402550694004db5b92c013
Vaibhavee <valandvaibhavee6423@gmail.com>
2024-12-31 20:13:39 +0530

    initial commit

---
+++ lib/common/Font.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Textstyle1Light18 {
+  static const TextStyle appbartextstyle = TextStyle(
+
+    fontFamily: 'OpenSans-Italic',
+    //color: Appcolors.greylight
+  );
+}
+
+class Textstyle2Light18 {
+  static const TextStyle appbartextstyle = TextStyle(
+
+      fontFamily: 'OpenSans-VariableFont',
+      /*color: Appcolors.greenlight*/
+  );
+}
+
+class Textstyle3Light18 {
+  static const TextStyle appbartextstyle = TextStyle(
+
+    fontFamily: 'Montserrat',
+    //color: Appcolors.greylight
+  );
+}
+
+class Textstyle4Light18 {
+  static const TextStyle appbartextstyle = TextStyle(
+
+    fontFamily: 'IBMPlexSans-Regular',
+    //color: Appcolors.greylight
+  );
+}
+
+class Textstyle5Light18 {
+  static const TextStyle appbartextstyle = TextStyle(
+    fontSize: 13,
+    fontFamily: 'Metropolis-Bold',
+    //color: Appcolors.greylight
+  );
+}
