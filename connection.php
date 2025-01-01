b4c528afe4ca5aab2c8f1adde09a5420c2f551bc
Vaibhavee06 <158445550+Vaibhavee06@users.noreply.github.com>
2025-01-01 12:37:00 +0530

    updated php files
    connection, login, signup, and validate file updated

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$serverHost="localhost";
+$user= "root";
+$password="";
+$database="fooddelivery_app";
+
+$connectNow = new mysqli($serverHost,$user,$password,$database);
---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+include "../connection.php";
+
+
+$userEmail=$_POST['user_email'];
+$userPassword=md5($_POST['user_password']);
+
+$sqlQuery="SELECT * FROM users_table WHERE user_email='$userEmail' AND user_password='$userPassword'";
+
+$resultQuery= $connectNow->Query($sqlQuery);
+if($resultQuery->num_rows>0)
+{
+    $userRecord=array();
+    while($rowFound=$resultQuery->fetch_assoc())
+    {
+       $userRecord[]= $rowFound;
+    }
+    echo json_encode(
+        array(
+            "success"=>true,
+            "userData"=>$userRecord[0],
+));
+}
+else
+{
+    echo json_encode(array("success"=>false));
+}
